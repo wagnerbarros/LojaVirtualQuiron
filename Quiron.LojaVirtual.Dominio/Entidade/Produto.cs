380c3f64bd059adbebcf5bb277c521f17282b850
wagnerbarros <wagnerbarros@outlook.com>
2014-07-16 15:37:15 -0300

    Classes paginacao

--- Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
+++ Quiron.LojaVirtual.Dominio/Entidade/Produto.cs
@@ -8,14 +8,11 @@ namespace Quiron.LojaVirtual.Dominio.Entidade
 {
     public  class Produto
     {
-
         public int ProdutoId { get; set; }
-
         public string  Nome { get; set; }
         public string  Descricao { get; set; }
+        public string Categoria { get; set; }
         public decimal Preco { get; set; }
-
-        public string  Categoria { get; set; }
 
     }
 }
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Quiron.LojaVirtual.Dominio.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public  class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+
+    }
+}
